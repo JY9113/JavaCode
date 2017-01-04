f27dda35a750b473a59c1f692fa16f358b20310a
JY <jiyoungahn9113@gmail.com>
2017-01-04 13:53:23 +0900

    1st

---
+++ lap-java/Java01-Intro/src/edu/java/intro/IntroMain.java
@@ -0,0 +1,13 @@
+package edu.java.intro;
+
+public class IntroMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java!");
+		System.out.println("안녕");
+		// 여기는 어떻게 보일까요? - 주석(comment)
+
+
+	} // end main()
+
+} // end class IntroMain
---
+++ lap-java/Java01-Intro/src/edu/java/intro2/PrintMain.java
@@ -0,0 +1,33 @@
+package edu.java.intro2;
+
+public class PrintMain {
+
+	public static void main(String[] args) {
+		// 주석(comment):
+		// 프로그램의 실행과는 상관 없고,
+		// 소스 코드의 설명을 추가하는 용도로 함.
+		/*
+		 * block comment(여러줄 주석)
+		 * 
+		 */
+
+		System.out.println("1 + 2 = 3");
+		System.out.println(1 + 2);
+		System.out.println("1 + 2 = " + (1 + 2));
+		System.out.println("1 + 2 = " + 1 + 2);
+		
+		// 사칙 연산(+, -, *, /)
+		System.out.println(100 + 200);
+		System.out.println(123 - 456);
+		System.out.println(9 * 9);
+
+
+	} // end main()
+
+} // end class PrintMain
+
+
+
+
+
+
